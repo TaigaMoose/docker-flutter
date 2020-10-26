7e595766f3a86f31746c16978dbb1730ad07c57a
loskutov-surf <loskutov@surfstudio.ru>
2020-10-26 19:09:16 +0300

    Add checking if $1 argument is 'flutter' and avoid

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# execute flutter with arguments
-flutter $1 $2 $3 $4 $5 $6
+# check if $1 == 'flutter' and execute flutter with arguments
+if  [[ $1 = "flutter" ]]
+then
+  $1 $2 $3 $4 $5 $6
+else
+  flutter $1 $2 $3 $4 $5 $6   
+fi
 
 /bin/bash /usr/local/bin/chown.sh
 
